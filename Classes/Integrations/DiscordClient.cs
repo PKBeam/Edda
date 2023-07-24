e908126d602fbdbd98298bddc903f462e670f8ab
Adam Zimny <neon349@gmail.com>
2023-07-24 18:08:01 +0200

    Inconsistent whitespace in DiscordClient.cs

--- Classes/Integrations/DiscordClient.cs
+++ Classes/Integrations/DiscordClient.cs
@@ -4,70 +4,68 @@ using DiscordRPC;
 
 public class DiscordClient {
     DiscordRpcClient client;
-	DateTime startTime;
-	bool enabled = false;
-	public DiscordClient() {
-		UpdateStartTime();
-	}
+    DateTime startTime;
+    bool enabled = false;
+    public DiscordClient() {
+        UpdateStartTime();
+    }
 
-	public void UpdateStartTime() {
-		startTime = DateTime.UtcNow;
-	}
+    public void UpdateStartTime() {
+        startTime = DateTime.UtcNow;
+    }
     public void SetPresence() {
-		SetPresence(null, 0);
+        SetPresence(null, 0);
     }
     public void SetPresence(string songName, int numNotes) {
-		if (!enabled) {
-			return;
+        if (!enabled) {
+            return;
         }
-		var rp = new RichPresence() {
-			Details = GetPresenceDetails(songName),
-			State = songName == null ? "" : $"{numNotes} notes placed",
-			Assets = new Assets() {
-				LargeImageKey = Edda.Const.DiscordRPC.IconKey
-			}
-		}.WithTimestamps(new Timestamps(startTime));
+        var rp = new RichPresence() {
+            Details = GetPresenceDetails(songName),
+            State = songName == null ? "" : $"{numNotes} notes placed",
+            Assets = new Assets() {
+                LargeImageKey = Edda.Const.DiscordRPC.IconKey
+            }
+        }.WithTimestamps(new Timestamps(startTime));
 
-		client.SetPresence(rp);
-	}
-
-	private string GetPresenceDetails(string songName)
-	{
-		switch (songName)
-		{
-			case null: return "No song open";
-			case "": return "Working on an untitled map";
-			default:
-				return $"Mapping: {songName}";
-
-        }
+        client.SetPresence(rp);
     }
 
-	public void Enable() {
-		var wasEnabled = enabled;
-		enabled = true;
-		if (!wasEnabled)
-		{
-			InitClient();
-		}
+    private string GetPresenceDetails(string songName)
+    {
+        return songName switch
+        {
+            null => "No song open",
+            "" => "Working on an untitled map",
+            _ => $"Mapping: {songName}",
+        };
     }
-	public void Disable() {
-		var wasEnabled = enabled; 
-		enabled = false;
-		if (wasEnabled)
-		{
+
+    public void Enable() {
+        var wasEnabled = enabled;
+        enabled = true;
+        if (!wasEnabled)
+        {
+            InitClient();
+        }
+    }
+    public void Disable() {
+        var wasEnabled = enabled; 
+        enabled = false;
+        if (wasEnabled)
+        {
             DeinitClient();
         }
-	}
-	private void InitClient() {
-		client = new DiscordRpcClient(Edda.Const.DiscordRPC.AppID);
-		client.Initialize();
-		SetPresence();
-	}
-	private void DeinitClient() {
-		if (client != null) {
-			client.Deinitialize();
-			client = null;
-		}
-	}
+    }
+    private void InitClient() {
+        client = new DiscordRpcClient(Edda.Const.DiscordRPC.AppID);
+        client.Initialize();
+        SetPresence();
+    }
+    private void DeinitClient() {
+        if (client != null) {
+            client.Deinitialize();
+            client = null;
+        }
+    }
 }
