a58f4025cab91f1efbc382750d96560d08bd6596
Adam Zimny <neon349@gmail.com>
2023-09-07 17:44:31 +0200

    PR comments from Nytilde.

--- Classes/MapEditor/Stats/MapStats.cs
+++ Classes/MapEditor/Stats/MapStats.cs
@@ -45,51 +45,32 @@ namespace Edda.Classes.MapEditor.Stats {
         }
 
         public void RecalculateNotes(SortedSet<Note> notes, SortedSet<Note> selectedNotes) {
-            this.allNotes = notes.Count;
+            allNotes = notes.Count;
             this.selectedNotes = selectedNotes.Count;
-            this.singleNotes = CalculateRowNotes(notes, 1);
-            this.doubleNotes = CalculateRowNotes(notes, 2);
+            singleNotes = CalculateRowNotes(notes, 1);
+            doubleNotes = CalculateRowNotes(notes, 2);
         }
 
-        private int CalculateRowNotes(SortedSet<Note> notes, int rowCount) {
-            int rowNoteCount = 0;
-            var noteIt = notes.GetEnumerator();
-            Note? lineStartNote = null;
-            var lineNoteCount = 0;
-
-
-            while (noteIt.MoveNext()) {
-                if (lineStartNote == null || !Helper.DoubleApproxEqual(noteIt.Current.beat, lineStartNote.beat)) {
-                    if (lineNoteCount == rowCount) {
-                        rowNoteCount++;
-                    }
-                    lineStartNote = noteIt.Current;
-                    lineNoteCount = 1;
-                } else {
-                    lineNoteCount++;
-                }
-            }
-            // last row to consider after all the notes were processed
-            if (lineNoteCount == rowCount) {
-                rowNoteCount++;
-            }
-
-            return rowNoteCount;
+        private static int CalculateRowNotes(SortedSet<Note> notes, int rowCount) {
+            return notes
+                .GroupBy(note => note.beat, new Helper.DoubleApproxEqualComparer())
+                .Select(group => group.Count())
+                .Count(count => count == rowCount);
         }
 
         public void RecalculateColumnVariety(SortedSet<Note> notes) {
             for (int i = 0; i < 4; i++) {
-                this.columnCounts[i] = notes.Where(note => note.col == i).Count();
-                this.columnPercentages[i] = double.Round(100 * (double)this.columnCounts[i] / Math.Max(this.allNotes, 1), 1);
+                columnCounts[i] = notes.Where(note => note.col == i).Count();
+                columnPercentages[i] = double.Round(100 * (double)columnCounts[i] / Math.Max(allNotes, 1), 1);
             }
         }
 
         public void RecalculateNPS(SortedSet<Note> notes) {
-            this.npsSong = double.Round(this.allNotes / this.songDuration, 2);
-            this.npsMapped = CalculateNPSMapped(notes);
-            this.nps16Beat = CalculateNPSPeakBeat(notes, 16);
-            this.nps8Beat = CalculateNPSPeakBeat(notes, 8);
-            this.nps4Beat = CalculateNPSPeakBeat(notes, 4);
+            npsSong = double.Round(allNotes / songDuration, 2);
+            npsMapped = CalculateNPSMapped(notes);
+            nps16Beat = CalculateNPSPeakBeat(notes, 16);
+            nps8Beat = CalculateNPSPeakBeat(notes, 8);
+            nps4Beat = CalculateNPSPeakBeat(notes, 4);
         }
 
         private double CalculateNPSPeakBeat(SortedSet<Note> notes, double beats) {
@@ -109,14 +90,14 @@ namespace Edda.Classes.MapEditor.Stats {
                 noteIndex++;
             }
 
-            return double.Round(peakNPB * this.globalBPM / 60, 2);
+            return double.Round(peakNPB * globalBPM / 60, 2);
         }
 
         private double CalculateNPSMapped(SortedSet<Note> notes) {
             double startBeat = notes.FirstOrDefault(new Note()).beat;
             double endBeat = notes.LastOrDefault(new Note()).beat;
             if (Helper.DoubleApproxGreater(endBeat, startBeat)) {
-                return double.Round(this.allNotes * this.globalBPM / ((endBeat - startBeat) * 60), 2);
+                return double.Round(allNotes * globalBPM / ((endBeat - startBeat) * 60), 2);
             } else {
                 return 0; // We need notes in at least two different beat positions to have non-zero map length
             }
