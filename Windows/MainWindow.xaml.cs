5e7faa9ca7e2467ea48efeff159316f8d56eeef3
Adam Zimny <neon349@gmail.com>
2023-09-12 18:34:12 +0200

    Moved song tempo parameters into constants. Fixed a small inconsistency where song tempo slider was not disabled during initialization phase.

--- Windows/MainWindow.xaml.cs
+++ Windows/MainWindow.xaml.cs
@@ -559,7 +559,7 @@ namespace Edda {
 
             // init difficulty-specific UI 
             SwitchDifficultyMap(0);
-            sliderSongTempo.Value = 1.0;
+            SetupSongTempoSlider();
             UpdateDifficultyButtons();
             DrawEditorGrid();
             scrollEditor.ScrollToBottom();
@@ -584,6 +584,7 @@ namespace Edda {
             btnPickCover.IsEnabled = true;
             sliderSongVol.IsEnabled = true;
             sliderDrumVol.IsEnabled = true;
+            sliderSongTempo.IsEnabled = true;
             checkMetronome.IsEnabled = true;
             checkGridSnap.IsEnabled = true;
             txtDifficultyNumber.IsEnabled = true;
@@ -618,6 +619,7 @@ namespace Edda {
             btnPickCover.IsEnabled = false;
             sliderSongVol.IsEnabled = false;
             sliderDrumVol.IsEnabled = false;
+            sliderSongTempo.IsEnabled = false;
             checkMetronome.IsEnabled = false;
             checkGridSnap.IsEnabled = false;
             txtDifficultyNumber.IsEnabled = false;
@@ -872,6 +874,13 @@ namespace Edda {
         }
 
         // map difficulties
+        private void SetupSongTempoSlider() {
+            sliderSongTempo.Minimum = Audio.MinSongTempo;
+            sliderSongTempo.Maximum = Audio.MaxSongTempo;
+            sliderSongTempo.SmallChange = Audio.SongTempoSmallChange;
+            sliderSongTempo.LargeChange = Audio.SongTempoLargeChange;
+            sliderSongTempo.Value = Audio.DefaultSongTempo;
+        }
         public void UpdateDifficultyButtons() {
             var numDiff = mapEditor.numDifficulties;
 
