8703d89f941b3316cf48c7ef3e9211d5b718c68d
Adam Zimny <neon349@gmail.com>
2024-11-07 22:50:45 +0100

    Version bump to 1.2.7 in beta to allow for some testing of new features before proper release.

--- Classes/EddaConstants.cs
+++ Classes/EddaConstants.cs
@@ -13,7 +13,7 @@ namespace Edda.Const {
         public const string Name = "Edda";
         public const string RepositoryURL = "https://github.com/PKBeam/Edda";
         public const string ReleasesAPI = "https://api.github.com/repos/PKBeam/Edda/releases";
-        public const string BaseVersionString = "1.2.6";
+        public const string BaseVersionString = "1.2.7b1";
         public const string VersionString =
 #if DEBUG
             BaseVersionString + "-dev";
